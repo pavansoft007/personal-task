b3d2bb07b7bd0d143ad745311da4ec610de8e47b
NGamerHere <codingdatta@gmail.com>
2024-09-25 16:09:52 +0530

    convent the entire code from common module to esm module

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const main=require('./routes/main')
+import express from "express";
+import cors from "cors";
+import Main from './routes/Main.js'
+const app = express()
+
 
 app.use(cors());
 app.use(express.json());
-app.use(main)
+app.use(Main);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Backend is running on port ${PORT}`);
 });
